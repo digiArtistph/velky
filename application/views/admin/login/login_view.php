5fcfe94e45f3b27630718edd9fba6ce3c0801ecb
juntals01 <juntals01@gmail.com>
2012-11-24 22:12:56 +0800

    Password Recovery updated

--- application/views/admin/login/login_view.php
+++ application/views/admin/login/login_view.php
@@ -17,7 +17,7 @@
 	 <div class="colleft">
     	<ul>
         	<li><input id="rememberme" type="checkbox" name="rememberme" /> <label for="rememberme">Remember me</label></li>
-            <li><p><a href="<?php echo base_url(). 'admin/password_recovery/forgot_password/' . strencode('admin');?>">Forgot Password?</a></p></li>
+            <li><p><a href="<?php echo base_url(). 'admin/password_recovery/reset_password';?>">Forgot Password?</a></p></li>
         </ul>
     </div>
     
