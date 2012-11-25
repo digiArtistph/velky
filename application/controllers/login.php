60235e6675d290063461570c2bbedf3061289f3c
epoy913 <danncliff@gmail.com>
2012-11-25 12:44:25 +0800

    adds login module for users

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,7 +2,7 @@
 
 class Login extends CI_Controller {
 	
-private $_mfullname;
+	private $_mfullname;
 	private $_mAdminFullname;
 	private $_mMyLoginError;
 	
@@ -14,107 +14,84 @@ private $_mfullname;
 		$this->_mMyLoginError = null;
 		
 	}
-	
 
 	public function index()
 	{
 		$this->load->view('view_login');
 	}
 	
-	function main_page ()
+	function main_page()
 	{
-		echo 'You are Logged in!';		
-	}
-	
-	public function logout(){
-		$this->session->sess_destroy();
+		echo 'You are Logged in! Hello!';
+		echo '<br /><a href="signout">Logout!</a>';
 		
 	}
-	public function validate_my_login()
-	{
-			$this->load->library('form_validation');
+public function validate_login() {
+		$this->load->library('form_validation');
 		$validation = $this->form_validation;
 		
-		$validation->set_rules('email', 'Email',  'required');		
+		$validation->set_rules('email', 'Email',  'required');
 		$validation->set_rules('password', 'Password', 'required');
 		
+		//call_debug($_POST);
 		
-// 		call_debug($_POST);
 		if($validation->run() === FALSE) {
 			$this->index();
 		} else {
-
-			if($this->_isUserExists()) {
+			if($this->__isUserExists()) {
 				$params = array(
-									'suser_uname' => $this->input->post('email'),
-									'suser_islog' => TRUE,
-									'suser_fullname' => $this->_mfullname
-								);
+						'suser_uname' => $this->input->post('email'),
+						'suser_islog' => TRUE,
+						'suser_fullname' => $this->_mfullname
+				);
 				
 				$this->sessionbrowser->setInfo($params);
-					
-				redirect('main_page');
-						
-			} else {
-				$this->_mMyLoginError = TRUE;
-				$this->index();
-			}
-		}
-	}
-/*		if($validation->run() == FALSE)
-		{
-			$this->index();	
-			//$data['title'] = $this->title;
-			//$data['main_content'] = 'admin/admin_view';
-			//$this->load->view('includes/template', $data);
-		}
-		else
-		{					
-			//fail		
-			//call_debug($_POST);
-			$this->load->model('user_model');
-			$result = $this->user_model->check_login();
-			if (!$result){			
-				redirect('login/validate_my_login');
-			}			
-			else {
 				
-				//logged in
-				$params = array(
-					'suser_uname' => $this->input->post('email'),
-					'suser_islog' => TRUE,
-					'admin_fullname' => $this->_mfullname
-					);
-								
-				$this->sessionbrowser->setInfo($params);
-				
-				$params = array('sadmin_uname', 'sadmin_islog','sadmin_fullname');
+				$params = array('suser_uname', 'suser_islog', 'suser_fillname');
 				$this->sessionbrowser->getInfo($params);
 				$arr = $this->sessionbrowser->mData;
 				
-				$this->session->set_userdata($data);
 				redirect('login/main_page');
-				//dari sa nako gi direct
 				
-			} 
-		} */
-
-private function _isUserExists() {
+			} else {
+				$this->index();
+				echo 'Username or password is invalid, try again.';
+				
+			}
+						
+		}
 		
-		$strQry = sprintf("SELECT * FROM `users` WHERE email='%s' AND password='%s'", $this->input->post('email'), MD5($this->input->post('password')));
+	}
 
+private function __isUserExists() {
+		
+		$strQry = sprintf("SELECT * FROM users WHERE email='%s' AND password='%s' AND utype='1'", $this->input->post('email'), MD5($this->input->post('password')));
+		
 		$this->load->model('mdldata');
 		$params['querystring'] = $strQry;
 		$this->mdldata->select($params);
-
+	
 		if($this->mdldata->_mRowCount < 1)
-		return FALSE;
-
+			return FALSE;
+		
 		foreach($this->mdldata->_mRecords as $rec) {
-			$this->_mAdminFullname = $rec->fname . ' ' . $rec->lname;
+			$this->_mfullname = $rec->fname . ' ' . $rec->lname;
 		}
-
-		}
+		
+		return TRUE;
 	}
+public function signout() {
+	
+		$params = array('suser_uname', 'suser_islog', 'suser_fillname');
+		$this->sessionbrowser->getInfo($params); // returns TRUE if successful, otherwise FALSE
+		$arr = $this->sessionbrowser->mData;
+	
+		$this->sessionbrowser->destroy($params);
+	
+		redirect(base_url() .'login');
+	
+	}
+	
+}
 	
 ?>
