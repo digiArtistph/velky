45d138c754191cfb86d28ef95bc1266506f71171
epoy913 <danncliff@gmail.com>
2012-11-30 08:58:41 +0800

    Fixes addhospital_view and addbarangay_view by adding fieldnames value to input types

--- application/views/admin/barangay/addbarangay_view.php
+++ application/views/admin/barangay/addbarangay_view.php
@@ -1,7 +1,7 @@
 <div>
 	<h3>Add Barangay</h3>
 	<?php echo form_open( base_url() . 'master/barangay/validatenewbarangay' );?>
-		<p><label>Name: </label><input type="text" name="name" /><span><?php echo form_error('name'); ?></span></p>
+		<p><label>Name: </label><input type="text" name="name" value="<?php echo set_value('name');?>" /><span><?php echo form_error('name'); ?></span></p>
 		<p><input type="submit" value="Submit barangay"/>
 		<input type="reset" value="Reset"/></p>
 	<?php echo form_close();?>
--- application/views/admin/hospitals/addhospital_view.php
+++ application/views/admin/hospitals/addhospital_view.php
@@ -1,9 +1,9 @@
 <div>
 	<h3>Add Hospitals</h3>
 	<?php echo form_open( base_url() . 'master/hospitals/validatenewhospital' );?>
-		<p><label>Name: </label><input type="text" name="name" /><span><?php echo form_error('name'); ?></span></p>
-		<p><label>Address: </label><input type="text" name="address" /><span><?php echo form_error('address'); ?></span></p>
-		<p><label>Phone: </label><input type="text" name="phone" /><span><?php echo form_error('phone'); ?></span></p>
+		<p><label>Name: </label><input type="text" name="name" value="<?php echo set_value('name');?>"/><span><?php echo form_error('name'); ?></span></p>
+		<p><label>Address: </label><input type="text" name="address" value="<?php echo set_value('address');?>" /><span><?php echo form_error('address'); ?></span></p>
+		<p><label>Phone: </label><input type="text" name="phone" value="<?php echo set_value('phone');?>"/><span><?php echo form_error('phone'); ?></span></p>
 
 		<p><input type="submit" value="Add Hospital"/>
 		<input type="reset" value="Reset"/></p>
