088b06afede3e13b347038b1acef8e676db88605
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2013-01-26 00:20:37 +0800

    adds sidebar and user logged status on the theme template

---
+++ application/views/includes/sidebar.php
@@ -0,0 +1,20 @@
+<ul class="menu">	
+    <li><a href="#">Modules</a>
+    	<ul class="menu">
+        	<li><a href="#">Accident Report</a></li>
+            <li><a href="#">Broadcast</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Reports</a></li>
+    <li><a href="#">Configurations</a></li>
+    <li><a href="#">Utility</a>
+    	<ul class="menu">
+        	<li><a href="#">Hospitals</a></li>
+            <li><a href="#">Police</a></li>
+            <li><a href="#">RTA</a></li>
+            <li><a href="#">Barangay</a></li>
+            <li><a href="#">Accident Type</a></li>
+        	<li><a href="#">Users</a></li>            
+        </ul>
+    </li>
+</ul>
