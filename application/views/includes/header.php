cf9fc6d63e6021cc61dd8830c96bdbdb5a917860
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2013-01-30 10:12:50 +0800

    integrating other sections on the template

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,38 +1,61 @@
+<?php if($section == 'home' || $section == "master"):; ?>
+    <!-- Bootstrap framework -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap.min.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap-responsive.min.css'); ?>" />
+    <!-- gebo blue theme-->
+        <link rel="stylesheet" href="<?php echo base_url('theme/css/blue.css" id="link_theme'); ?>" />
+    <!-- breadcrumbs-->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/jBreadcrumbs/css/BreadCrumb.css'); ?>" />
+    <!-- tooltips-->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/qtip2/jquery.qtip.min.css'); ?>" />
+    <!-- notifications -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/sticky/sticky.css'); ?>" />
+    <!-- notifications -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/sticky/sticky.css'); ?>" />
+    <!-- splashy icons -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/img/splashy/splashy.css'); ?>" />
+    <!-- colorbox -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/colorbox/colorbox.css'); ?>" />	
+    <!-- smoke_js -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/lib/smoke/themes/gebo.css'); ?>" />
+    
+    <!-- main styles -->
+        <link rel="stylesheet" href="<?php echo base_url('theme/css/style.css'); ?>" />
+        
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans" />
+    
+    <!-- Favicon -->
+        <link rel="shortcut icon" href="<?php echo base_url('theme/img/favicon.ico'); ?>" />
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo base_url('theme/css/ie.css'); ?>" />
+        <script src="<?php echo base_url('theme/js/ie/html5.js'); ?>"></script>
+        <script src="<?php echo base_url('theme/js/ie/respond.min.js'); ?>"></script>
+    <![endif]-->
+    
+    <script>
+        //* hide all elements & show preloader
+        document.documentElement.className += 'js';
+    </script>
+
+<?php elseif($section == "admin"): ?>
 <!-- Bootstrap framework -->
     <link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap.min.css'); ?>" />
     <link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap-responsive.min.css'); ?>" />
-<!-- gebo blue theme-->
-    <link rel="stylesheet" href="<?php echo base_url('theme/css/blue.css" id="link_theme'); ?>" />
-<!-- breadcrumbs-->
-    <link rel="stylesheet" href="<?php echo base_url('theme/lib/jBreadcrumbs/css/BreadCrumb.css'); ?>" />
-<!-- tooltips-->
+    <!-- theme color-->
+    <link rel="stylesheet" href="<?php echo base_url('theme/css/blue.css'); ?>" />
+    <!-- tooltip -->    
     <link rel="stylesheet" href="<?php echo base_url('theme/lib/qtip2/jquery.qtip.min.css'); ?>" />
-<!-- notifications -->
-    <link rel="stylesheet" href="<?php echo base_url('theme/lib/sticky/sticky.css'); ?>" />
-<!-- notifications -->
-    <link rel="stylesheet" href="<?php echo base_url('theme/lib/sticky/sticky.css'); ?>" />
-<!-- splashy icons -->
-    <link rel="stylesheet" href="<?php echo base_url('theme/img/splashy/splashy.css'); ?>" />
-<!-- colorbox -->
-    <link rel="stylesheet" href="<?php echo base_url('theme/lib/colorbox/colorbox.css'); ?>" />	
-<!-- smoke_js -->
-    <link rel="stylesheet" href="<?php echo base_url('theme/lib/smoke/themes/gebo.css'); ?>" />
-
-<!-- main styles -->
+    <!-- main styles -->
     <link rel="stylesheet" href="<?php echo base_url('theme/css/style.css'); ?>" />
     
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans" />
-
-<!-- Favicon -->
+    <!-- Favicon -->
     <link rel="shortcut icon" href="<?php echo base_url('theme/img/favicon.ico'); ?>" />
-
-<!--[if lte IE 8]>
-    <link rel="stylesheet" href="<?php echo base_url('theme/css/ie.css'); ?>" />
+    
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+    
+    <!--[if lte IE 8]>
     <script src="<?php echo base_url('theme/js/ie/html5.js'); ?>"></script>
     <script src="<?php echo base_url('theme/js/ie/respond.min.js'); ?>"></script>
-<![endif]-->
-
-<script>
-    //* hide all elements & show preloader
-    document.documentElement.className += 'js';
-</script>
+    <![endif]-->
+<?php endif; ?>
--- application/views/includes/navigation.php
+++ application/views/includes/navigation.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="url" content="<?php echo base_url();?>" />
-    <title><?php echo (isset($title)) ? $title : ''; ?></title>
-    <?php getHeader(); ?>
-</head>
-    
-<body>
-	<div id="loading_layer" style="display:none"><img src="<?php echo base_url('theme/img/ajax_loader.gif'); ?>" alt="" /></div>
-    <div id="maincontainer">
-        <div class="navbar navbar-fixed-top">
-        	<!--//top fixed navigation bar-->
-            <!--<div class="navbar navbar-fixed-top">
+<div class="navbar navbar-fixed-top">
                     <div class="navbar-inner">
                         <div class="container-fluid">
                             <a class="brand" href="dashboard.html"><i class="icon-home icon-white"></i> Gebo Admin</a>
@@ -107,62 +92,4 @@
 							</ul>
                         </div>
                     </div>
-                </div>-->
-            <?php $this->load->view('includes/navigation'); ?>
-        </div>
-        
-        <div id="contentwrapper">
-            <div class="main_content">
-            	<?php $this->load->view($main_content); ?>
-            </div>
-        </div>
-    	
-        <a href="javascript:void(0)" class="sidebar_switch on_switch ttip_r" title="Hide Sidebar">Sidebar switch</a>
-        <div class="sidebar">
-	        <!--//sidebar content-->
-            <?php getSideBar(); ?>
-        </div>
-        
-    	<!--//js scripts-->
-        <script src="<?php echo base_url('theme/js/jquery.min.js'); ?>"></script>
-			<!-- smart resize event -->
-			<script src="<?php echo base_url('theme/js/jquery.debouncedresize.min.js'); ?>"></script>
-			<!-- hidden elements width/height -->
-			<script src="<?php echo base_url('theme/js/jquery.actual.min.js'); ?>"></script>
-			<!-- js cookie plugin -->
-			<script src="<?php echo base_url('theme/js/jquery_cookie.min.js'); ?>"></script>
-			<!-- main bootstrap js -->
-			<script src="<?php echo base_url('theme/bootstrap/js/bootstrap.min.js'); ?>"></script>
-			<!-- tooltips -->
-			<script src="<?php echo base_url('theme/lib/qtip2/jquery.qtip.min.js'); ?>"></script>
-			<!-- jBreadcrumbs -->
-			<script src="<?php echo base_url('theme/lib/jBreadcrumbs/js/jquery.jBreadCrumb.1.1.min.js'); ?>"></script>
-			<!-- sticky messages -->
-            <script src="<?php echo base_url('theme/lib/sticky/sticky.min.js'); ?>"></script>
-			<!-- fix for ios orientation change -->
-			<script src="<?php echo base_url('theme/js/ios-orientationchange-fix.js'); ?>"></script>
-			<!-- scrollbar -->
-			<script src="<?php echo base_url('theme/lib/antiscroll/antiscroll.js'); ?>"></script>
-			<script src="<?php echo base_url('theme/lib/antiscroll/jquery-mousewheel.js'); ?>"></script>
-			<!-- lightbox -->
-            <script src="<?php echo base_url('theme/lib/colorbox/jquery.colorbox.min.js'); ?>"></script>
-			<!-- mobile nav -->
-			<script src="<?php echo base_url('theme/js/selectNav.js'); ?>"></script>
-            <!-- common functions -->
-			<script src="<?php echo base_url('theme/js/gebo_common.js'); ?>"></script>
-            
-			<!-- smoke_js -->
-			<script src="<?php echo base_url('theme/lib/smoke/smoke.js'); ?>"></script>
-            <!-- notifications functions -->
-            <script src="<?php echo base_url('theme/js/gebo_notifications.js'); ?>"></script>
-    
-			<script>
-				$(document).ready(function() {
-					//* show all elements & remove preloader
-					setTimeout('$("html").removeClass("js")',1000);
-				});
-			</script>
-    </div>
-
-</body>
-</html>
+                </div>
