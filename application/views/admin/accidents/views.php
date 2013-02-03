f9567f44a2557e363f3c9837a88bf1ed03701377
wkhu <windylkhu@gmail.com>
2013-02-04 01:20:14 +0800

    edits view_by_day.php

--- application/views/admin/accidents/views.php
+++ application/views/admin/accidents/views.php
@@ -2,11 +2,11 @@
 <div>
 	<h3>Reports Views</h3>
 	<ul class="menu">
-        	<li><a class="ext_disabled" href="<?php echo base_url('admin/loginad/dayaccidents'); ?>">View by Day</a></li>
-            <li><a class="ext_disabled" href="<?php echo base_url('admin/loginad/weekaccidents'); ?>">View by Week</a></li>
-            <li><a class="ext_disabled" href="<?php echo base_url('admin/loginad/monthaccidents'); ?>">View by Month</a></li>
-            <li><a class="ext_disabled" href="<?php echo base_url('admin/loginad/yearaccidents'); ?>">View by Year</a></li>
-            <li><a class="ext_disabled" href="<?php echo base_url('admin/loginad/barangayaccidents'); ?>">View by Barangay</a></li>
+        	<li><a class="ext_disabled" href="<?php echo base_url('reports/accidents/section/dayaccidents'); ?>">View by Day</a></li>
+            <li><a class="ext_disabled" href="<?php echo base_url('reports/accidents/section/weekaccidents'); ?>">View by Week</a></li>
+            <li><a class="ext_disabled" href="<?php echo base_url('reports/accidents/section/monthaccidents'); ?>">View by Month</a></li>
+            <li><a class="ext_disabled" href="<?php echo base_url('reports/accidents/section/yearaccidents'); ?>">View by Year</a></li>
+            <li><a class="ext_disabled" href="<?php echo base_url('reports/accidents/section/barangayaccidents'); ?>">View by Barangay</a></li>
     </ul>
 	
 </div>
